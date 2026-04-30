058ec823be7bfa09517a12498e598dce83586282
Nikhil Pandey <nikhilpandey1033@gmail.com>
2026-04-30 20:10:35 +0530

    fix: add vercel url in cors

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000
 
 //Middlewares
 app.use(cors({
-        origin:"http://localhost:5173"
+        origin:"https://bird-bot.vercel.app"
 }))
 app.use(express.json())
 
